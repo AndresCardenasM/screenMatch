c71c0bdf7a44185ea66375f3ac5ca182aff4d8c7
Andres Felipe Muñoz Cardenas <143211280+AndresCardenasM@users.noreply.github.com>
2024-04-09 10:37:05 -0500

    mainWClassIn.java

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,13 @@ public class Main {
         miMovie.name = "Encanto";
         miMovie.releaseDate = 2021;
         miMovie.durationInMinutes = 120;
+        miMovie.includedInThePlan= true;
 
-        System.out.println("Mi pelicula es: " + miMovie.name);
-        System.out.println("Su fecha de lanzamiento fue: " + miMovie.releaseDate);
+        miMovie.showDataSheet();
+        miMovie.evalua(3);
+        miMovie.evalua(6);
+        miMovie.evalua(10);
+        System.out.println("Média de evaluaciones de la película: " +miMovie.calculaMedia());
 
         Movie newMovie = new Movie();
         newMovie.name = "Matrix";
