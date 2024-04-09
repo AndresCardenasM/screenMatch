66addb70a8d2d80ba89d332251c4bd0f43d9ff12
Andres Felipe Muñoz Cardenas <143211280+AndresCardenasM@users.noreply.github.com>
2024-04-09 10:36:37 -0500

    movieWClass.java

--- src/Movie.java
+++ src/Movie.java
@@ -2,4 +2,20 @@ public class Movie {
     String name;
     int releaseDate;
     int durationInMinutes;
+    boolean includedInThePlan;
+    double evaluationsSum;
+    int evaluationsTotal;
+
+    void showDataSheet(){
+        System.out.println("El nombre de la pelicula es: "+ name);
+        System.out.println("Su fecha de lanzamiento  fue: "+ releaseDate);
+        System.out.println("Duracion en minutos de la pelicula: " + durationInMinutes);
+    }
+    void evalua(double nota){
+        evaluationsSum += nota;
+        evaluationsTotal++;
+    }
+    double calculaMedia(){
+        return evaluationsSum / evaluationsTotal;
+    }
 }
